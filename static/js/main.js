d99927d02d8ac6ccaf512e5c7e37976b424bc250
MatviievaViktoriia <vika030718@gmail.com>
2019-10-24 11:12:57 +0300

    descktop-version

---
+++ static/js/main.js
@@ -0,0 +1,37 @@
+
+
+(function() {
+  'use strict';
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+
+  // Smooth Scroll
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+
+})();
+
+
+  
