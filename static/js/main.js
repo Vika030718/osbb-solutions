655bc8fa75d87c74ee54fc89d322373de47372c1
MatviievaViktoriia <vika030718@gmail.com>
2019-10-24 14:50:16 +0300

    last commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,22 @@
+$(function() {
 
-
-(function() {
-  'use strict';
-
-  // Example starter JavaScript for disabling form submissions if there are invalid fields
-  window.addEventListener('load', function() {
-    var forms = document.getElementsByClassName('needs-validation');
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
+    $(".menu__toggler").on("click", function(e) {
+        $(".menu__main").toggleClass("menu_opened");
+        console.log("dsfsd")
     });
-  }, false);
 
-  // Smooth Scroll
-  $("a").on('click', function(event) {
-
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    } 
-  });
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
 
 })();
 
